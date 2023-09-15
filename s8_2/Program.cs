f64b7118c38cfc4f11813bd4afa8f6c2c04db877
Yulia <meshokyulia0002@gmail.com>
2023-09-15 15:31:29 +0300

    s82

---
+++ s8_2/Program.cs
@@ -0,0 +1,85 @@
+﻿int rows = InputNumbers("Введите количество строк: "); 
+int cols = InputNumbers("Введите количество столбцов: ");
+int[,] array = new int[rows, cols];
+CreateArray(array);
+PrintArray(array);
+
+int[,] minElement = new int[1,2];
+minElement = FindminElement(array,minElement);
+
+
+int[,] arrayWithoutLines = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+DeleteLines(array, minElement, arrayWithoutLines);
+Console.WriteLine($"Получившийся массив:");
+PrintArray(arrayWithoutLines);
+
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1,10);
+    }
+  }
+}
+
+void PrintArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] FindminElement(int[,] array, int[,] position)
+{
+  int temp = array[0, 0];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i, j] <= temp)
+      {
+        position[0, 0] = i;
+        position[0, 1] = j;
+        temp = array[i, j];
+      }
+    }
+  }
+  Console.WriteLine($"Mинимальный элемент: {temp}");
+  return position;
+}
+
+void DeleteLines(int[,] array, int[,] minElement, int[,] arrayWithoutLines)
+{
+  int k = 0, l = 0;
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (minElement[0, 0] != i && minElement[0, 1] != j)
+      {
+        arrayWithoutLines[k, l] = array[i, j];
+        l++;
+      }
+    }
+    l = 0;
+    if (minElement[0, 0] != i)
+    {
+      k++;
+    }
+  }
+}
